dd19703726f7b924d8577e3e72fc96720da267a2
Niko77g <94113127+Niko77g@users.noreply.github.com>
2024-04-21 13:23:52 +0200

    Add Ftracer file

---
+++ Ftracer/_internal/classOOP.py
@@ -0,0 +1,8 @@
+class Data:
+    def __init__(self, exercise, number_of_repetitions, number_of_series, weight):
+        self.exercise = exercise
+        self.number_of_repetitions = number_of_repetitions
+        self.number_of_series = number_of_series
+        self.weight = weight
+  
+        
